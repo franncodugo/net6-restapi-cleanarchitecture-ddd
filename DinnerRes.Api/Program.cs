68245f42d82b5a4d4fdb854947537e7463d9097c
Franco Dugo Vidal <francodugo.vidal@gmail.com>
2023-04-10 17:12:47 -0300

    Completing the ErrorEndpoint error handling approach with ProblemDetails Override

--- DinnerRes.Api/Program.cs
+++ DinnerRes.Api/Program.cs
@@ -1,9 +1,11 @@
 using System.ComponentModel;
+using DinnerRes.Api.Errors;
 using DinnerRes.Api.Filters;
 using DinnerRes.Api.Middleware;
 using DinnerRes.Application;
 using DinnerRes.Application.Authentication;
 using DinnerRes.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -11,12 +13,17 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
 
-    builder.Services.AddControllers( opt => opt.Filters.Add<ErrorHandlingFilterAttribute>());
+    //builder.Services.AddControllers( opt => opt.Filters.Add<ErrorHandlingFilterAttribute>());
+    builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, DinnerResProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
     //app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
+    
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
